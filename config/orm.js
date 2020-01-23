3b5564c7ddc2bb3393c8ddd07ca41f84fbd7d325
Kacie Hatley <kaciehatley@gmail.com>
2020-01-23 15:39:23 -0500

    added orm and connection js files into config folder. created mysql connection and added orm containing methods to select all burgers, add a burger, and update a burger.

---
+++ config/orm.js
@@ -0,0 +1,63 @@
+// Import MySQL connection.
+var connection = require("../config/connection");
+
+// * `selectAll()`
+// * `insertOne()`
+// * `updateOne()`
+
+// Retrieve all data on all burgers in burgers_db
+
+
+// Object for all our SQL statement functions.
+var orm = {
+    selectAll: function(tableInput, cb) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    insertOne: function(table, cols, vals, cb) {
+      var queryString = "INSERT INTO " + table;
+  
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
+      queryString += ") ";
+  
+      console.log(queryString);
+  
+      connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+    // An example of objColVals would be {name: panther, sleepy: true}
+    updateOne: function(table, objColVals, condition, cb) {
+      var queryString = "UPDATE " + table;
+  
+      queryString += " SET ";
+      queryString += objToSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    }
+  };
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
